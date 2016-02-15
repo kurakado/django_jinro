97e156eb30955c3a0ca4ba6258e477bf37531711
kurakado <you@example.com>
2016-02-15 22:22:31 +0900

    prepare static files

--- myTestSite/myTestSite/settings.py
+++ myTestSite/myTestSite/settings.py
@@ -83,6 +83,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/root/myDjango/staticFiles"
 
 TEMPLATE_DIRS = (
     '/root/myDjango/myTestSite_templates',
